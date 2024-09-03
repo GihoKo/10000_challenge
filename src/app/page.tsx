4f94d3e9a1dc031426617319b4a886a50871a141
giho <giho7427@daum.net>
2024-09-03 22:46:49 +0900

    design: 로그인 페이지 디자인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <>init</>;
+    return (
+        <>
+            <Link href={"/signIn"}>Sign In</Link>
+            <Link href={"/signUp"}>Sign Up</Link>
+            <Link href={"/dashboard"}>Dashboard</Link>
+        </>
+    );
 }
