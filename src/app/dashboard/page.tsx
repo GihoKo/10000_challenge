077491ed2e137185b30522df10cb60400129eaa3
giho <giho7427@daum.net>
2024-09-05 23:41:15 +0900

    design: dashboard 페이지 간단 코드

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,18 @@
 export default function Dashboard() {
-    return <>dashboard</>;
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-3.5">챌린지 대시보드</h2>
+
+            <div>챌린지 생성</div>
+
+            <div>
+                {/* 챌린지는 총 5개까지 만들 수 있음 */}
+                <div>챌린지1</div>
+                <div>챌린지2</div>
+                <div>챌린지3</div>
+                <div>챌린지4</div>
+                <div>챌린지5</div>
+            </div>
+        </div>
+    );
 }
