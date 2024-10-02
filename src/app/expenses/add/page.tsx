3a30c002c4b86263b31819a2bd9bc8f3b83e3d22
giho <giho7427@daum.net>
2024-10-02 19:22:21 +0900

    chore: 지출 추가 페이지에 Main 컴포넌트 추가

--- src/app/expenses/add/page.tsx
+++ src/app/expenses/add/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import ConfirmButton from "@/components/button/ConfirmButton";
-import ExpenseCategorySelect from "@/components/input/ExpenseCategorySelect";
 import PageContentHeader from "@/components/Header/PageContentHeader";
-import useAddPage from "./_components/Main.hook";
-import DesciptionInput from "./_components/DesciptionInput";
-import AmountInput from "./_components/AmountInput";
-import DateInput from "./_components/DateInput";
+import Main from "./_components/Main";
 
 export default function Add() {
     return (
         <div>
             <PageContentHeader text="지출 추가하기" />
+
+            <Main />
         </div>
     );
 }
