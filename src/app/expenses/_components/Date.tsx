4fac00b646a44f8595ffe15cc3c5dfca3b1f34bd
giho <giho7427@daum.net>
2024-10-02 14:46:28 +0900

    feat: expenses 페이지 날짜 변경 기능 추가

--- src/app/expenses/_components/Date.tsx
+++ src/app/expenses/_components/Date.tsx
@@ -1,9 +1,35 @@
+"use client";
+
+import { useDateStore } from "@/stores/dateStore";
+
 export default function Date() {
+    const { date, setDate } = useDateStore();
+
+    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDate(e.target.value);
+    };
+
+    const formatDate = (date: string) => {
+        // YYYY-MM-DD를 YYYY년 MM월 DD일으로 변경
+        const year = date.slice(0, 4);
+        const month = date.slice(5, 7);
+        const day = date.slice(8, 10);
+
+        return `${year}년 ${month}월 ${day}일`;
+    };
+
     return (
         <div className="text-sm mb-10">
             오늘은
             <span className="text-xl font-medium mx-1 text-blue-700">
-                2024월 09월 05일
+                {formatDate(date)}
+                {/* input의 달력 이미지만 남기고 글자는 지우기 */}
+                <input
+                    type="date"
+                    onChange={handleDateChange}
+                    value={date}
+                    className="text-transparent w-6"
+                />
             </span>
             이에요.
         </div>
