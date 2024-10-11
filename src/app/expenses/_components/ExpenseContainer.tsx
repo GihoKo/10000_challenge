9f8cd73c9841fac5b35641418201a117cd4983a8
giho <giho7427@daum.net>
2024-10-11 14:23:49 +0900

    feat: 리팩토링 주석 생성

--- src/app/expenses/_components/ExpenseContainer.tsx
+++ src/app/expenses/_components/ExpenseContainer.tsx
@@ -4,6 +4,10 @@ import useExpenseContainer from "./ExpenseContainer.hook";
 import Expense from "./Expense";
 import { ExpenseData } from "@/types/expense";
 
+// 서버 클라이언트로 사용해 stream으로 후순위로 렌더링
+// 1. 모달 삭제해서 expense edit 페이지 생성할 것
+// 2. hook을 사용하지 않고, 비동기 데이터를 관리할 것
+// 3. zustand를 여기서 사용하지 않을 것
 export default function ExpenseContainer() {
     const { expenses } = useExpenseContainer();
 
