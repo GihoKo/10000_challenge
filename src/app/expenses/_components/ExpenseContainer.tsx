b0eabe33bf2a253ed8449295e187f6490e93482e
giho <giho7427@daum.net>
2024-10-11 14:21:52 +0900

    useExpenseContainer의 return 값들 중 필요 없는 것 삭제

--- src/app/expenses/_components/ExpenseContainer.tsx
+++ src/app/expenses/_components/ExpenseContainer.tsx
@@ -3,38 +3,17 @@
 import useExpenseContainer from "./ExpenseContainer.hook";
 import Expense from "./Expense";
 import { ExpenseData } from "@/types/expense";
-import EditExpenseModal from "./EditExpenseModal";
 
 export default function ExpenseContainer() {
-    const {
-        expenses,
-        isModalOpen,
-        modalExpenseId,
-        updateExpense,
-        deleteExpense,
-        handleClickExpense,
-        handleModalClose,
-    } = useExpenseContainer();
+    const { expenses } = useExpenseContainer();
 
     return (
         <div>
             <ul className="flex flex-col gap-2">
                 {expenses.map((expense: ExpenseData) => (
-                    <Expense
-                        key={expense.id}
-                        expense={expense}
-                        onClick={handleClickExpense}
-                    />
+                    <Expense key={expense.id} expense={expense} />
                 ))}
             </ul>
-            {isModalOpen && (
-                <EditExpenseModal
-                    modalExpenseId={modalExpenseId}
-                    updateExpense={updateExpense}
-                    deleteExpense={deleteExpense}
-                    handleModalClose={handleModalClose}
-                />
-            )}
         </div>
     );
 }
