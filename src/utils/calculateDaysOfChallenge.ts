330a9f5b674f35c59ff749725a899a99d99543fb
giho <giho7427@daum.net>
2024-11-12 09:35:53 +0900

    fix: 챌린지 기간 넘어가는 버그 수정

--- src/utils/calculateDaysOfChallenge.ts
+++ src/utils/calculateDaysOfChallenge.ts
@@ -2,17 +2,30 @@ import { ChallengeResponse } from "@/types/challenge";
 
 export const cacultateDaysOfChallenge = (challenge: ChallengeResponse) => {
     const nowDayMilliseconds = Number(new Date().getTime());
+    let progressDayMilliseconds = 0;
+    let totalDayMilliseconds = 0;
+
     const startDayMilliseconds = Number(
         new Date(challenge.start_date).getTime()
     );
     const goalDayMilliseconds = Number(new Date(challenge.goal_date).getTime());
 
-    const progressDayMilliseconds = Number(
-        nowDayMilliseconds - startDayMilliseconds
-    );
-    const totalDayMilliseconds = Number(
-        goalDayMilliseconds - startDayMilliseconds
-    );
+    if (nowDayMilliseconds >= goalDayMilliseconds) {
+        progressDayMilliseconds = Number(
+            goalDayMilliseconds - startDayMilliseconds
+        );
+        totalDayMilliseconds = Number(
+            goalDayMilliseconds - startDayMilliseconds
+        );
+    } else {
+        progressDayMilliseconds = Number(
+            nowDayMilliseconds - startDayMilliseconds
+        );
+
+        totalDayMilliseconds = Number(
+            goalDayMilliseconds - startDayMilliseconds
+        );
+    }
 
     const progressDays = Math.floor(progressDayMilliseconds / 86400000) + 1;
     const totalDays = Math.floor(totalDayMilliseconds / 86400000) + 1;
