395dd95be205c9dc8d66208cbecb9d50f775b43d
giho <giho7427@daum.net>
2024-11-22 14:33:04 +0900

    feat: Home 페이지에 og 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,33 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const BASE_URL =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://10000-challenge.vercel.app";
+
 export const metadata: Metadata = {
+    metadataBase: new URL(BASE_URL), // Metadata을 기반으로 og태그의 image를 생성하려면 metadataBase를 설정해야 한다.
     title: "만원 챌린지",
     description: "만원 챌린지로 소비 습관을 바꿔보세요.",
     robots: "follow, index",
+    openGraph: {
+        siteName: "만원 챌린지",
+        locale: "ko_KR",
+        title: "클릭시 바로 이동합니다!",
+        description: "만원 챌린지로 소비 습관을 바꿔보세요.",
+        type: "website",
+        url: BASE_URL,
+        images: [
+            {
+                url: "/logo512.png", // public 폴더를 기준으로 한다.
+                alt: "로고 이미지",
+                type: "image/png",
+                width: 1200,
+                height: 630,
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
