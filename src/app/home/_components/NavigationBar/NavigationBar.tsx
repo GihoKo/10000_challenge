5180a5ce78493b3e0131c496bb7afd7bbe93381f
giho <giho7427@daum.net>
2024-12-03 15:21:10 +0900

    refactor: NAVIGATION_ITEMS 상수로 분리

--- src/app/home/_components/NavigationBar/NavigationBar.tsx
+++ src/app/home/_components/NavigationBar/NavigationBar.tsx
@@ -1,40 +1,13 @@
-import homeSvg from "@/images/svg/home-black.svg";
-import moneySvg from "@/images/svg/money.svg";
-import challengeSvg from "@/images/svg/challenge.svg";
 import ImageWrapper from "@/components/ImageWrapper";
+import NAVIGATION_ITEMS from "@/constants/NAVIGATION_ITEMS";
 import Link from "next/link";
-import settingSvg from "@/images/svg/setting-black.svg";
-
-// @ToDo 설정 페이지 구현
-const navigationItems = [
-    {
-        name: "홈",
-        href: "/home",
-        image: homeSvg,
-    },
-    {
-        name: "지출",
-        href: "/home/expenses",
-        image: moneySvg,
-    },
-    {
-        name: "챌린지",
-        href: "/home/previous-challenges",
-        image: challengeSvg,
-    },
-    {
-        name: "세팅",
-        href: "/home/setting",
-        image: settingSvg,
-    },
-];
 
 export default function NavigationBar() {
     return (
         <div className="max-w-[600px] w-full bg-white fixed bottom-0 z-10">
             <div className="rounded-t-xl bg-blue-100 w-full h-8"></div>
             <div className="flex justify-evenly items-center w-full">
-                {navigationItems.map((item) => (
+                {NAVIGATION_ITEMS.map((item) => (
                     <Link
                         key={item.name}
                         href={item.href}
--- src/app/home/setting/page.tsx
+++ src/app/home/setting/page.tsx
@@ -1,3 +1,4 @@
+// @ToDo 설정 페이지 구현
 export default function Setting() {
     return <div>설정 페이지</div>;
 }
