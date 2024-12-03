cb5ac55714efa3190bcfb4ca485a1ef27fb64708
giho <giho7427@daum.net>
2024-12-03 13:34:21 +0900

    refactor: expenses, previous-challenges 페이지 layout 수정 및 home 화면에서의 라우팅 경로 수정

--- src/app/home/expenses/layout.tsx
+++ src/app/home/expenses/layout.tsx
@@ -1,3 +1,9 @@
+import AnimatedPage from "@/components/animated/animatedPage";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <div className="flex flex-col py-8 px-4">{children}</div>;
+    return (
+        <div>
+            <AnimatedPage>{children}</AnimatedPage>
+        </div>
+    );
 }
