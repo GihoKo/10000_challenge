80cb0e3368f490693a6f6b6b94a14f6be92369fb
giho <giho7427@daum.net>
2024-12-04 10:09:40 +0900

    feat: DeleteChallengeModal 구현

--- src/app/home/challenge/[challengeId]/_componenets/DeleteChallengeModal.tsx
+++ src/app/home/challenge/[challengeId]/_componenets/DeleteChallengeModal.tsx
@@ -1,37 +1,47 @@
 import NagativeButton from "@/components/button/NagativeButton";
-import { ChallengeDeleteModalProps } from "./DeleteChallengeModal.type";
-import DangerousButton from "@/components/button/DangerousButton";
+import ModalName from "@/components/Modal/ModalName";
+import ModalDescription from "@/components/Modal/ModalDescription";
+import { deleteChallenge } from "@/apis/services/challenge";
+import { useParams, useRouter } from "next/navigation";
+import useModalStore from "@/stores/modalStore";
+import ConfirmButton from "@/components/button/ConfirmButton";
 
-export default function DeleteChallengeModal({
-    onClose,
-    handleDeleteChallenge,
-}: ChallengeDeleteModalProps) {
+export default function DeleteChallengeModal() {
+    const { closeModal } = useModalStore();
+    const { challengeId } = useParams();
+    const router = useRouter();
+
+    const handleCloseModal = () => {
+        closeModal();
+    };
+
+    const handleDelete = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        deleteChallenge({ challengeId })
+            .then(() => {
+                closeModal();
+                router.push("/home/challenge");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-            <div className="w-full flex flex-col gap-2 p-4 bg-white rounded-md mx-6">
-                <span>챌린지 삭제 하시겠습니까?</span>
+        <div>
+            <ModalName text="챌린지 삭제" />
 
-                <div className="flex justify-end gap-2">
-                    <NagativeButton
-                        type="button"
-                        text="취소"
-                        rounded="rounded-md"
-                        px="px-2"
-                        py="py-1"
-                        width="w-auto"
-                        onClick={onClose}
-                    />
-                    <DangerousButton
-                        type="button"
-                        text="삭제"
-                        rounded="rounded-md"
-                        px="px-2"
-                        py="py-1"
-                        width="w-auto"
-                        onClick={handleDeleteChallenge}
-                    />
-                </div>
-            </div>
+            <ModalDescription text="챌린지를 삭제하시겠습니까?" />
+
+            <form onSubmit={handleDelete} className="flex gap-2 mt-4">
+                <ConfirmButton type="submit" text="삭제" bg="bg-red-600" />
+                <NagativeButton
+                    type="button"
+                    text="취소"
+                    onClick={handleCloseModal}
+                />
+            </form>
         </div>
     );
 }
