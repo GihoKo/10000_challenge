90f5db00c3f38f142538a2fbd657234cb305e40d
giho <giho7427@daum.net>
2024-12-05 16:56:18 +0900

    design: main 내부 컨텐츠 데이터 바인딩 할 것들 작성

--- src/app/home/setting/_components/Main.tsx
+++ src/app/home/setting/_components/Main.tsx
@@ -1,9 +1,21 @@
 export default function Main() {
     return (
         <main>
-            <div>유저 프로필</div>
+            <div className="flex flex-col gap-4 py-4">
+                <div className="font-bold">유저 정보</div>
+                <div>유저 이름</div>
+                <div>유저 이메일</div>
+                <div>로그아웃</div>
+            </div>
 
-            <div>소비 카테고리 설정</div>
+            <div className="w-full h-1 bg-gray-200 my-2" />
+
+            <div className="flex flex-col gap-2">
+                <div className="font-bold">소비 카테고리</div>
+                <div>카테고리 영역</div>
+                {/** 버튼 클릭시 모달창이 열려 Create,Delete */}
+                <div>카테고리 관리 버튼</div>
+            </div>
         </main>
     );
 }
