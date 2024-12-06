fc848bdabfd2676769157ec410f845f8a4dc053b
giho <giho7427@daum.net>
2024-12-06 14:37:18 +0900

    refactor: ExpenseCategory의 업데이트, 삭제 버튼 이벤트 위임으로 리팩토링

--- src/app/home/setting/expenseCategory/_components/ExpenseCategory.tsx
+++ src/app/home/setting/expenseCategory/_components/ExpenseCategory.tsx
@@ -1,73 +1,22 @@
 import ImageWrapper from "@/components/ImageWrapper";
 import deleteSvg from "@/images/svg/delete-black.svg";
 import editSvg from "@/images/svg/edit-black.svg";
-import { ExpenseCategory as ExpenseCategoryInterface } from "./Main.type";
-import UpdateExpenseCategoryModal from "./UpdateExpenseCategoryModal";
-import DeleteExpenseCategoryModal from "./DeleteExpenseCategoryModal";
-import useModalStore from "@/stores/modalStore";
-import { ExpenseCategoryAction } from "@/reducers/expenseCategoryReducer";
+import { ExpenseCategoryProps } from "./ExpenseCategory.type";
 
-export interface ExpenseCategoryProps {
-    category: ExpenseCategoryInterface;
-    expenseCategoriesDispatch: React.Dispatch<ExpenseCategoryAction>;
-}
-
-export default function ExpenseCategory({
-    category,
-    expenseCategoriesDispatch,
-}: ExpenseCategoryProps) {
-    const { setIsModalOpen } = useModalStore();
-
-    const handleUpdateCategoryModalOpenButtonClick = (
-        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) => {
-        const currentExpenseCategoryName = e.currentTarget.dataset.name;
-        const currentExpenseCategoryId = Number(e.currentTarget.dataset.id);
-
-        if (!currentExpenseCategoryName || !currentExpenseCategoryId) return;
-
-        const currentExpenseCategory = {
-            id: currentExpenseCategoryId,
-            name: currentExpenseCategoryName,
-        };
-
-        setIsModalOpen(
-            <UpdateExpenseCategoryModal
-                currentExpenseCategory={currentExpenseCategory}
-                expenseCategoriesDispatch={expenseCategoriesDispatch}
-            />
-        );
-    };
-
-    const handleDeleteCategoryModalOpenButtonClick = (
-        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) => {
-        const currentExpenseCategoryId = Number(e.currentTarget.dataset.id);
-
-        if (!currentExpenseCategoryId) return;
-
-        setIsModalOpen(
-            <DeleteExpenseCategoryModal
-                currentExpenseCategoryId={currentExpenseCategoryId}
-                expenseCategoriesDispatch={expenseCategoriesDispatch}
-            />
-        );
-    };
-
+export default function ExpenseCategory({ category }: ExpenseCategoryProps) {
     return (
         <li
             key={category.id}
-            className="flex justify-between items-center bg-gray-100 p-2 rounded-lg
-"
+            className="flex justify-between items-center bg-gray-100 p-2 rounded-lg"
         >
             <span className="text-base">{category.name}</span>
             <div className="flex gap-2">
                 <button
                     data-name={category.name}
                     data-id={category.id}
+                    data-type="update"
                     className="border border-gray-300 rounded-lg p-2 bg-white"
                     type="button"
-                    onClick={handleUpdateCategoryModalOpenButtonClick}
                 >
                     <ImageWrapper
                         src={editSvg}
@@ -80,7 +29,7 @@ export default function ExpenseCategory({
                     className="border border-gray-300  rounded-lg p-2 bg-white"
                     type="button"
                     data-id={category.id}
-                    onClick={handleDeleteCategoryModalOpenButtonClick}
+                    data-type="delete"
                 >
                     <ImageWrapper
                         src={deleteSvg}
--- src/app/home/setting/expenseCategory/_components/ExpenseCategoryContainer.tsx
+++ src/app/home/setting/expenseCategory/_components/ExpenseCategoryContainer.tsx
@@ -1,13 +1,76 @@
 import NoExpenseCategory from "./NoExpenseCategory";
 import ExpenseCategory from "./ExpenseCategory";
 import { ExpenseCategoryContainerProps } from "./ExpenseCategoryContainer.type";
+import useModalStore from "@/stores/modalStore";
+import UpdateExpenseCategoryModal from "./UpdateExpenseCategoryModal";
+import DeleteExpenseCategoryModal from "./DeleteExpenseCategoryModal";
 
 export default function ExpenseCategoryContainer({
     expenseCategories,
     expenseCategoriesDispatch,
 }: ExpenseCategoryContainerProps) {
+    const { setIsModalOpen } = useModalStore();
+
+    const handleUpdateCategoryModalOpenButtonClick = (e: React.MouseEvent) => {
+        const currentExpenseCategoryName = (
+            e.target as HTMLElement
+        ).closest<HTMLButtonElement>("button")?.dataset.name;
+
+        const currentExpenseCategoryId = Number(
+            (e.target as HTMLElement).closest<HTMLButtonElement>("button")
+                ?.dataset.id
+        );
+
+        if (!currentExpenseCategoryName || !currentExpenseCategoryId) return;
+
+        const currentExpenseCategory = {
+            id: currentExpenseCategoryId,
+            name: currentExpenseCategoryName,
+        };
+
+        setIsModalOpen(
+            <UpdateExpenseCategoryModal
+                currentExpenseCategory={currentExpenseCategory}
+                expenseCategoriesDispatch={expenseCategoriesDispatch}
+            />
+        );
+    };
+
+    const handleDeleteCategoryModalOpenButtonClick = (e: React.MouseEvent) => {
+        const currentExpenseCategoryId = Number(
+            (e.target as HTMLElement).closest<HTMLButtonElement>("button")
+                ?.dataset.id
+        );
+
+        if (!currentExpenseCategoryId) return;
+
+        setIsModalOpen(
+            <DeleteExpenseCategoryModal
+                currentExpenseCategoryId={currentExpenseCategoryId}
+                expenseCategoriesDispatch={expenseCategoriesDispatch}
+            />
+        );
+    };
+
+    const handleUpdateOrDeleteCategoryButtonClick = (e: React.MouseEvent) => {
+        const buttonType = (e.target as HTMLElement).closest<HTMLButtonElement>(
+            "button"
+        )?.dataset.type;
+
+        if (!buttonType) return;
+
+        if (buttonType === "update") {
+            handleUpdateCategoryModalOpenButtonClick(e);
+        } else if (buttonType === "delete") {
+            handleDeleteCategoryModalOpenButtonClick(e);
+        }
+    };
+
     return (
-        <ul className="flex flex-col gap-2">
+        <ul
+            className="flex flex-col gap-2"
+            onClick={handleUpdateOrDeleteCategoryButtonClick}
+        >
             {expenseCategories.length !== 0 ? (
                 expenseCategories.map((category) => {
                     return (
