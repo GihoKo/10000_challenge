022aa2d4924063ccda517ff7cf05c0121cf849ef
giho <giho7427@daum.net>
2024-12-08 17:13:51 +0900

    refactor: 정리
    (cherry picked from commit e66da4b04c77e7262e1c5dac0c62947f5f213539)

--- src/app/home/expenses/_components/ExpenseContainer/ExpenseContainer.tsx
+++ src/app/home/expenses/_components/ExpenseContainer/ExpenseContainer.tsx
@@ -1,23 +1,42 @@
+"use client";
+
 import Expense from "../Expense/Expense";
 import { ExpenseData } from "@/types/expense";
 import { getExpensesByDate } from "@/apis/services/expense";
 import { useDateStore } from "@/stores/dateStore";
+import { useEffect, useState } from "react";
 
-// SSR + streaming을 이용하기
-export default async function ExpenseContainer() {
-    const { date } = useDateStore.getState();
+// CSR
+export default function ExpenseContainer() {
+    const [expenses, setExpenses] = useState<ExpenseData[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const { date } = useDateStore();
 
-    const expenses = await getExpensesByDate({ date })
-        .then((response) => {
-            if (response) {
-                return response;
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    useEffect(() => {
+        setIsLoading(true);
 
-    if (!expenses) return <div>데이터를 가져오지 못했습니다.</div>;
+        getExpensesByDate({ date })
+            .then((response) => {
+                if (response) {
+                    setExpenses(response);
+                }
+            })
+            .catch(() => {
+                setIsError(true);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, [date]);
+
+    if (isLoading) {
+        return <div>데이터를 불러오는 중 입니다.</div>;
+    }
+
+    if (isError) {
+        return <div>데이터를 가져오지 못했습니다.</div>;
+    }
 
     return (
         <ul className="flex flex-col gap-2">
@@ -28,37 +47,21 @@ export default async function ExpenseContainer() {
     );
 }
 
-// CSR
-// export default function ExpenseContainer() {
-//     const [expenses, setExpenses] = useState<ExpenseData[]>([]);
-//     const [isLoading, setIsLoading] = useState(true);
-//     const [isError, setIsError] = useState(false);
-//     const { date } = useDateStore();
+// SSR + streaming을 이용하기
+// export default async function ExpenseContainer() {
+//     const { date } = useDateStore.getState();
 
-//     useEffect(() => {
-//         setIsLoading(true);
+//     const expenses = await getExpensesByDate({ date })
+//         .then((response) => {
+//             if (response) {
+//                 return response;
+//             }
+//         })
+//         .catch((error) => {
+//             console.error(error);
+//         });
 
-//         getExpensesByDate({ date })
-//             .then((response) => {
-//                 if (response) {
-//                     setExpenses(response);
-//                 }
-//             })
-//             .catch(() => {
-//                 setIsError(true);
-//             })
-//             .finally(() => {
-//                 setIsLoading(false);
-//             });
-//     }, [date]);
-
-//     if (isLoading) {
-//         return <div>데이터를 불러오는 중 입니다.</div>;
-//     }
-
-//     if (isError) {
-//         return <div>데이터를 가져오지 못했습니다.</div>;
-//     }
+//     if (!expenses) return <div>데이터를 가져오지 못했습니다.</div>;
 
 //     return (
 //         <ul className="flex flex-col gap-2">
