17e789aa3021cb18504d4325b3907c4b73d62be1
giho <giho7427@daum.net>
2024-12-08 13:17:27 +0900

    fix: 남은 금액 로직 수정

--- src/app/home/challenge/[challengeId]/_componenets/ChallengeInfo/ChallengeInfo.hook.tsx
+++ src/app/home/challenge/[challengeId]/_componenets/ChallengeInfo/ChallengeInfo.hook.tsx
@@ -5,7 +5,10 @@ import { getExpenseCategoryByChallengeId } from "@/apis/services/expenseCategory
 import { ExpenseCategory } from "@/app/home/setting/expenseCategory/_components/Main/Main.type";
 import { useParams } from "next/navigation";
 
-export default function useChallengeInfo({ challenge }: UseChallengeInfoProps) {
+export default function useChallengeInfo({
+    challenge,
+    expenses,
+}: UseChallengeInfoProps) {
     const { challengeId } = useParams() as { challengeId: string };
     const [expenseCategoriesOfChallenge, setExpenseCategoriesOfChallenge] =
         useState<ExpenseCategory[]>([]);
@@ -19,12 +22,14 @@ export default function useChallengeInfo({ challenge }: UseChallengeInfoProps) {
         if (!challenge) return 0;
 
         const totalSaving = Number(challenge.daily_saving) * progressDays;
-        const totalExpense = 10000;
+        const totalExpense = expenses.reduce((acc, cur) => {
+            return acc + cur.amount;
+        }, 0);
 
         const remainingSaving = Number(totalSaving - totalExpense);
 
         return remainingSaving;
-    }, [challenge, progressDays]);
+    }, [challenge, progressDays, expenses]);
 
     const calculateRemainingDays = useCallback(() => {
         const remainingDays = Number(totalDays - progressDays);
