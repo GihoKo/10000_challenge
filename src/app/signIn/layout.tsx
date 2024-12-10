3d073f1b0b4cdc706b677af1e48a90ccea934b3a
giho <giho7427@daum.net>
2024-12-10 15:07:16 +0900

    refactor: AnimatedPage 컴포넌트 삭제

--- src/app/signIn/layout.tsx
+++ src/app/signIn/layout.tsx
@@ -1,5 +1,3 @@
-import AnimatedPage from "@/components/animated/animatedPage";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <AnimatedPage>{children}</AnimatedPage>;
+    return <div>{children}</div>;
 }
