e1a3a772b94369561e55659ee7b5f39115821685
giho <giho7427@daum.net>
2024-12-10 10:51:14 +0900

    refacotr: renewUser 로직 api 폴더로 이동

--- src/components/User/RenewUser.tsx
+++ src/components/User/RenewUser.tsx
@@ -1,39 +1,16 @@
 "use client";
 
-import { useUserStore } from "@/stores/userStore";
-import supabaseClient from "@/supabase/client";
-import { UserMetadata } from "@supabase/supabase-js";
+import { renewUser } from "@/apis/services/auth";
 import { useEffect } from "react";
 
 export default function RenewUser() {
-    const { setUser } = useUserStore();
-
     useEffect(() => {
-        const getUser = async () => {
-            const {
-                data: { session },
-                error: sessionError,
-            } = await supabaseClient.auth.getSession();
-
-            if (sessionError || !session) {
-                console.error("No session found:", sessionError?.message);
-                return;
-            }
-
-            const {
-                data: { user },
-                error: userError,
-            } = await supabaseClient.auth.getUser();
-
-            if (userError) {
-                throw new Error(userError.message);
-            }
-
-            setUser(user?.user_metadata as UserMetadata);
-        };
-
-        getUser();
-    }, [setUser]);
+        renewUser()
+            .then(() => {})
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
     return null;
 }
