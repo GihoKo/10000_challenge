11ae080ece02d570c0fce888e2f0af365f6855e9
giho <giho7427@daum.net>
2024-12-10 15:39:37 +0900

    design: 로그인 페이지 화면 전환 추가

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -2,16 +2,19 @@ import Header from "@/components/sign/Header";
 import Logo from "@/components/sign/Logo";
 import SignInForm from "@/components/sign/SignInForm";
 import NavigateSignUpLink from "./_components/NavigateSignUpLink";
+import PageTransition from "@/components/animated/PageTransition";
 
 export default function SignIn() {
     return (
-        <div className="h-screen flex flex-col justify-center items-center gap-6 px-3">
-            <div className="w-full flex-col">
-                <Logo />
-                <Header />
-                <SignInForm />
-                <NavigateSignUpLink />
+        <PageTransition direction="up">
+            <div className="h-screen flex flex-col justify-center items-center gap-6 px-3">
+                <div className="w-full flex-col">
+                    <Logo />
+                    <Header />
+                    <SignInForm />
+                    <NavigateSignUpLink />
+                </div>
             </div>
-        </div>
+        </PageTransition>
     );
 }
