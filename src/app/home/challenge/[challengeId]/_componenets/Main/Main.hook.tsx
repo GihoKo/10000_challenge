630eabe3eccfa616750a33ff8946966f002fca86
giho <giho7427@daum.net>
2024-12-10 14:33:52 +0900

    refactor: 필요없는 코드 삭제

--- src/app/home/challenge/[challengeId]/_componenets/Main/Main.hook.tsx
+++ src/app/home/challenge/[challengeId]/_componenets/Main/Main.hook.tsx
@@ -3,24 +3,19 @@ import { getExpensesByChallengeDuration } from "@/apis/services/expense";
 import useModalStore from "@/stores/modalStore";
 import { ChallengeResponse } from "@/types/challenge";
 import { ExpenseData } from "@/types/expense";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import DeleteChallengeModal from "../DeleteChallengeModal/DeleteChallengeModal";
-import { useModal } from "@/contexts/ModalContext";
 
 export default function useMain() {
     const { challengeId } = useParams();
     const setIsModalOpen = useModalStore.getState().setIsModalOpen;
-    // const { setIsModalOpen, setContent } = useModal();
 
     const [challenge, setChallenge] = useState<ChallengeResponse>();
     const [expenses, setExpenses] = useState<ExpenseData[]>([]);
 
     const handleDeleteChallengeModalOpen = () => {
         setIsModalOpen(<DeleteChallengeModal />);
-
-        // setContent(<DeleteChallengeModal />);
-        // setIsModalOpen(true);
     };
 
     useEffect(() => {
